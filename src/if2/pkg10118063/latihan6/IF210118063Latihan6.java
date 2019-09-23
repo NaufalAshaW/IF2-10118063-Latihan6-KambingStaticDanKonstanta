92eab0590842a3e727dce594f78fadb076739632
NaufalAshaW <55681676+NaufalAshaW@users.noreply.github.com>
2019-09-23 19:54:28 +0700

    Add files via upload

---
+++ src/if2/pkg10118063/latihan6/IF210118063Latihan6.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if2.pkg10118063.latihan6;
+
+/**
+ *
+ * @author 
+ * NAMA   : Naufal Asha
+ * KELAS  : IF-2
+ * NIM    : 10118063
+ */
+
+public class IF210118063Latihan6 {
+    /**
+     * @param args the command line arguments
+     */
+    public static final String NAMA_KAMBING = "Naufal";
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Mamalia.jumlahKambing = 25300;
+        System.out.println(NAMA_KAMBING + " Memiliki kambing sebanyak: " +Mamalia.jumlahKambing);
+    }
+    
+}
+
---
+++ src/if2/pkg10118063/latihan6/Mamalia.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package if2.pkg10118063.latihan6;
+
+/**
+ *
+ * @author 
+ * NAMA   : Naufal Asha
+ * KELAS  : IF-2
+ * NIM    : 10118063
+ */
+public class Mamalia {
+    //variabel jumlah kambing
+    public static int jumlahKambing;
+}
